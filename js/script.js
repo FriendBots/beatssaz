c606ff43d49549120d345fc8842868a579865e20
Sanjeet Patel <sanjeetpatel4812@gmail.com>
2025-02-18 21:24:04 +0530

    added a new blog and fixed a ton of stuff

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 // If you need any JavaScript functionality, you can add it here.
 // Currently, it's empty as the site is mostly static.
+document.getElementById('hamburger').addEventListener('click', function() {
+  document.getElementById('nav-links').classList.toggle('open');
+});
 
